aa4e542441023de16912eba1c04d2dcd890e2854
Bushra Khugyani <160358779+bushrakhugyani@users.noreply.github.com>
2024-05-03 10:22:59 -0700

    added footer to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <header className="App-header">
        <h1> Weather Search Eingen in React</h1>
       </header>
+      <footer>
+         This project was coded by Bushra Khugyani and is
+        <a href="https://github.com/bushrakhugyani/react-weather"> open-sourced on GitHub
+        </a>
+      </footer>
+
     </div>
   );
 }
