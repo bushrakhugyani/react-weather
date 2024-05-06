0125a92be28ac71aababf985153d8965c2a4bbeb
Bushra Khugyani <160358779+bushrakhugyani@users.noreply.github.com>
2024-05-06 11:11:32 -0700

    added date on the weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,20 @@
 import React, { useState} from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 export default function Weather(props){
     
-    const[temperature , setTemperature]= useState({ready:false});
-    const[weatherData ,setWeatherData]=useState({});
+
+    const[weatherData ,setWeatherData]=useState({ready:false});
     function handleResponse(response){
-        console.log(response.data);
-        setTemperature(response.data.main.temp);
+        
+
         setWeatherData({
             ready: true,
+            date:new Date(response.data.dt *1000),
             temperature:response.data.main.temp,
             humidity:response.data.main.humidity,
-            date: "Wednesday 07:00",
+          
             description: response.data.weather[0].description,
             iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
             wind:response.data.wind.speed,
@@ -35,7 +37,9 @@ export default function Weather(props){
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-            <li>{weatherData.date}</li>
+            <li>
+                <FormattedDate date={weatherData.date} />
+            </li>
             <li className="text-capitalize">{weatherData.description}</li>
           </ul>
           <div className="row mt-3">
