8bd648d3ec935a0dee087e6274533717f6ab7c2e
Bushra Khugyani <160358779+bushrakhugyani@users.noreply.github.com>
2024-05-09 11:46:49 -0700

    added new component for weather temperature conversion and import it into WeatherInfo component.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props){
     return (
      <div className='WeatherInfo'>
@@ -18,8 +19,8 @@ export default function WeatherInfo(props){
                 <div className="flot-left">
                 <WeatherIcon code={props.data.icon} alt={props.data.description}/>
                </div>
-                <span className="temperature">{Math.round(props.data.temperature)}</span>
-                <span className="unit">°C</span>
+               <WeatherTemperature celsius={props.data.temperature}/>
+               
                 </div>
                
             </div>
