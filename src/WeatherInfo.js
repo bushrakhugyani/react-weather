8ecbdee491db87f50936cf06bb0487b637bae9f2
Bushra Khugyani <160358779+bushrakhugyani@users.noreply.github.com>
2024-05-10 12:45:35 -0700

    removed some comments from html file and also did some changes to css of weather app in react

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,18 +15,19 @@ export default function WeatherInfo(props){
           </ul>
           <div className="row mt-3">
             <div className="col-6">
-             <div className="position">
+             <div className="d-flex">
                 <div className="flot-left">
                 <WeatherIcon code={props.data.icon} size={52}/>
                </div>
+              
                <WeatherTemperature celsius={props.data.temperature}/>
-               
+              
                 </div>
                
-            </div>
+          </div>
            
             <div className="col-6">
-                <ul>
+                <ul >
                  
                     <li>Humidity:{props.data.humidity}%</li>
                     <li>Wind: {props.data.wind}km/h</li>
